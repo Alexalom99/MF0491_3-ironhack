ff4100ce3c7285945b62e5b9606e74f8336d6441
Alexalom99 <alexalom1999@gmail.com>
2025-04-07 16:43:02 +0200

    uf1842 DOM done

--- uf1842/2-DOM/js/app.js
+++ uf1842/2-DOM/js/app.js
@@ -1,3 +1,12 @@
 /**
  * ¡Buena suerte! ¡Pasa el mensaje!
  */
+
+const text = document.querySelector("#message");
+const btn = document.querySelector("#btn-send");
+const lastMessage = document.querySelector("#last-message");
+
+btn.addEventListener("click", function() {
+    lastMessage.textContent = text.value;
+    text.value = "";
+})
