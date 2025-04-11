d7fd3f520dc609a269e9a3317bbbed79ed84a0f2
Alexalom99 <alexalom1999@gmail.com>
2025-04-11 09:35:18 +0200

    done apis

--- uf1842/3-APIs/scripts/request.js
+++ uf1842/3-APIs/scripts/request.js
@@ -1,5 +1,10 @@
 async function getPuzzle(wordCount) {
     let hiddenSentence = "Buena Suerte";
+    let url = `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`;
+    
+    const response = await fetch(url);
+    const json = await response.json();
+    hiddenSentence = json.puzzle;
 
     return hiddenSentence;
 }
